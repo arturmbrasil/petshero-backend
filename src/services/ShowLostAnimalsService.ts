57105c97e6209c05548e3db8d9af48d10846b4c3
Artur Brasil <arturmbrasil@hotmail.com>
2021-03-08 22:37:38 -0300

    lost animals order by updated_at

--- src/services/ShowLostAnimalsService.ts
+++ src/services/ShowLostAnimalsService.ts
@@ -34,6 +34,7 @@ class ShowLostAnimalsService {
       animals = await lostAnimalsRepository.find({
         where: { owner_id },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (owner_name) {
       animals = await lostAnimalsRepository
@@ -44,6 +45,7 @@ class ShowLostAnimalsService {
         .where(`owner.name ILIKE :owner_name`, {
           owner_name: `%${owner_name}%`,
         })
+        .orderBy(`updated_at`, `DESC`)
         .getMany();
     } else if (animal_name) {
       animals = await lostAnimalsRepository
@@ -54,40 +56,48 @@ class ShowLostAnimalsService {
         .where(`lost_animals.name ILIKE :animal_name`, {
           animal_name: `%${animal_name}%`,
         })
+        .orderBy(`updated_at`, `DESC`)
         .getMany();
     } else if (age) {
       animals = await lostAnimalsRepository.find({
         where: { age },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (gender) {
       animals = await lostAnimalsRepository.find({
         where: { gender },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (size) {
       animals = await lostAnimalsRepository.find({
         where: { size },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (species) {
       animals = await lostAnimalsRepository.find({
         where: { species },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (breed) {
       animals = await lostAnimalsRepository.find({
         where: { breed },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else if (found) {
       animals = await lostAnimalsRepository.find({
         where: { found },
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     } else {
       animals = await lostAnimalsRepository.find({
         relations: ['owner', 'owner.address'],
+        order: { updated_at: 'DESC' },
       });
     }
 
